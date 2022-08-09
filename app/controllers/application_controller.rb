9f05d323c85d9a40e87287c41fddb87130b3dd8a
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-08-09 14:44:08 -0600

    sign up backedn created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  # before_action
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  def record_not_found(exception)
+    render json: { errors: ["#{exception.model} not found"]}, status: :not_found
+  end
+
+  def record_invalid(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only [:index]
   resources :movement_types, only: %i[index]
-  get "/cookie_click", to: "sessions#click"
 
     # namespace :api do
       # get "/cookie_click", to: "sessions#click"
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
       # post "/login", to: "sessions#create"
       # delete "/logout", to: "sessions#destroy"
     # end
+
+     #Authentication routes
+  post "/signup", to: "users#create"
   
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
