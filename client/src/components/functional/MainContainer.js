2809eb2bb2a7d3bf440e7cac5e2d37411f7d0925
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-08-18 01:49:01 -0600

    Nav bar sketched out

--- client/src/components/functional/MainContainer.js
+++ client/src/components/functional/MainContainer.js
@@ -3,10 +3,14 @@ import NavBar from "./NavBar";
 
 function MainContainer() {
   return (
-    <>
-      <NavBar />
-      <InformationContainer />
-    </>
+    <div class="grid container grid grid-cols-4 ">
+      <div class="item1 col-span-1">
+        <NavBar />
+      </div>
+      <div class="item2 col-span-3 ">
+        <InformationContainer />
+      </div>
+    </div>
   );
 }
 
--- client/src/components/functional/NavBar.js
+++ client/src/components/functional/NavBar.js
@@ -1,5 +1,53 @@
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
 function NavBar() {
-  return <h1>NavBar</h1>;
+  const [chosenPage, setChosenPage] = useState("My Movements");
+  const history = useHistory();
+  const userFullName = `${
+    JSON.parse(sessionStorage.getItem("user")).first_name
+  } ${JSON.parse(sessionStorage.getItem("user")).last_name}`;
+
+  function handleNavigation(e) {
+    e.preventDefault();
+    setChosenPage(e.target.textContent);
+    switch (e.target.textContent) {
+      case "My Movements":
+        history.push("/home");
+        break;
+      case "Record a Movement":
+        history.push("/home/tracker/movement");
+        break;
+    }
+  }
+
+  return (
+    <div class="flex flex-col gap-3  mt-20  sm:ml-10 ml-5">
+      <button
+        disabled
+        class="text-left text-white font-semibold text-sm sm:text-lg py-2 px-5"
+      >
+        {userFullName}
+      </button>
+      <div class="border-2 border-gray-200 rounded-lg"></div>
+      <button
+        class={`text-left text-white font-semibold text-sm sm:text-lg hover:bg-jungle_500 py-2 px-5 hover:rounded-xl ${
+          chosenPage === "My Movements" ? "bg-jungle_500 rounded-xl" : null
+        } `}
+        onClick={handleNavigation}
+      >
+        My Movements
+      </button>
+      <button
+        class={`text-left text-white font-semibold text-lg hover:bg-jungle_500 py-2 px-5 hover:rounded-xl ${
+          chosenPage === "Record a Movement" ? "bg-jungle_500 rounded-xl" : null
+        }`}
+        onClick={handleNavigation}
+      >
+        Record a Movement
+      </button>
+    </div>
+  );
 }
 
 export default NavBar;
